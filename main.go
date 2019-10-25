db42f4101ce62c4ee99455c541d40dfd94a95db9
Tarek Ali <tarekali15@outlook.com>
2019-10-25 09:02:24 -0500

    added second html template, writing the go code

--- main.go
+++ main.go
@@ -2,21 +2,28 @@ package main
 
 import (
 	"html/template"
-	"log"
 	"net/http"
 )
 
-var html *template.Template
+var (
+	html    *template.Template
+	htmlTwo *template.Template
+)
 
 func init() {
 	html = template.Must(template.ParseGlob("index.html"))
+	htmlTwo = template.Must(template.ParseGlob("length.html"))
 }
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		text := r.FormValue("text")
-		log.Printf("characters: %v", len(text))
-		html.ExecuteTemplate(w, "index.html", nil)
+		if len(text) > 0 {
+			htmlTwo.ExecuteTemplate(w, "length.html", nil)
+		} else {
+			html.ExecuteTemplate(w, "index.html", nil)
+		}
+
 	})
 	http.ListenAndServe(":8080", nil)
 }
