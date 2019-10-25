58bb399ad2b5cdce519ea0ae4aae6d69a8214819
Tarek Ali <tarekali15@outlook.com>
2019-10-25 09:13:54 -0500

    playing around with html templates

--- main.go
+++ main.go
@@ -6,20 +6,27 @@ import (
 )
 
 var (
-	html    *template.Template
-	htmlTwo *template.Template
+	html *template.Template
 )
 
 func init() {
 	html = template.Must(template.ParseGlob("index.html"))
-	htmlTwo = template.Must(template.ParseGlob("length.html"))
 }
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		text := r.FormValue("text")
 		if len(text) > 0 {
-			htmlTwo.ExecuteTemplate(w, "length.html", nil)
+			htmlTwo, err := template.New("").Parse(`
+				<h1>Length: 
+					{{range .len(text)}}	
+				</h1>
+			`)
+			if err != nil {
+				panic(err)
+			}
+			htmlTwo.Execute(w, htmlTwo)
+
 		} else {
 			html.ExecuteTemplate(w, "index.html", nil)
 		}
