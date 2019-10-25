367a370be5f3705ccca78b398f9a2a3b0a02051a
Tarek Ali <tarekali15@outlook.com>
2019-10-25 16:21:48 -0500

    ok this works for now

--- main.go
+++ main.go
@@ -1,35 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
-	"log"
 	"net/http"
-	"os"
 )
 
 var (
-	html  *template.Template`
+	html    *template.Template
+	htmlTwo *template.Template
 )
 
 func init() {
 	html = template.Must(template.ParseGlob("index.html"))
+	htmlTwo = template.Must(template.ParseGlob("length.html"))
 }
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		text := r.FormValue("text")
-		index = `{{define "length"}}
-		Length: {{.text}}
-		{{end}}`	
-		htmlTwo, err := template.New("length").Parse(index)
-		if err != nil {
-			log.Fatal(err)
-		}
 		if len(text) > 0 {
-			htmlTwo.ExecuteTemplate(os.Stdout, "title", nil)
+			fmt.Fprintf(w, "Length of text: %d", len(text))
 		} else {
 			html.ExecuteTemplate(w, "index.html", nil)
 		}
+
 	})
 	http.ListenAndServe(":8080", nil)
 
