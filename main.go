6eec16fb8d1c5d0fa82014245f9c77e848c860d5
Tarek Ali <tarekali15@outlook.com>
2019-10-25 19:24:44 -0500

    yay!

--- main.go
+++ main.go
@@ -4,23 +4,22 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"strings"
 )
 
 var (
-	html    *template.Template
-	htmlTwo *template.Template
+	html *template.Template
 )
 
 func init() {
 	html = template.Must(template.ParseGlob("index.html"))
-	htmlTwo = template.Must(template.ParseGlob("length.html"))
 }
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		text := r.FormValue("text")
 		if len(text) > 0 {
-			fmt.Fprintf(w, "Length of text: %d", len(text))
+			fmt.Fprintf(w, "Char Count: %d", len(countChars(text)))
 		} else {
 			html.ExecuteTemplate(w, "index.html", nil)
 		}
@@ -28,4 +27,8 @@ func main() {
 	})
 	http.ListenAndServe(":8080", nil)
 
+}
+
+func countChars(text string) string {
+	return strings.Replace(text, " ", "", -1)
 }
